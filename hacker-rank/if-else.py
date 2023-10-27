e9ba9e2073f142dcada523905515dff9c289f9b0
sp-howard <stevenphoward@gmail.com>
2023-10-27 13:03:24 -0700

    HackerRank - If-Else

---
+++ hacker-rank/if-else.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+# https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+
+if (n % 2 == 0):
+    if (n >= 2 and n <= 5):
+        print("Not Weird")
+    elif (n >= 6 and n <= 20):
+        print("Weird")
+    elif (n > 20):
+        print("Not Weird")
+else:
+    print("Weird")
